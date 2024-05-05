3f213f4f0c37329edd3d352e26fe109e062344ca
Giovanni Bernardo <canaltamojuntooooo@gmail.com>
2024-05-05 18:51:49 -0300

    feat: new navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle />
-    </ThemeProvider>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-12 py-3 flex items-center justify-between border-b">
+        <img src="/public/logo.png  " width={50} height={50} />
+
+        <div className="flex items-center gap-3">
+          <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <ModeToggle />
+          </ThemeProvider>
+
+          <a href="https://github.com/giovannibraaga" target="_blank">
+            <Button variant="outline">
+              <Github className="w-4 h-4 mr-2"/>
+              Github
+            </Button>
+          </a>
+        </div>
+      </div>
+      
+    </div>
   );
 }
